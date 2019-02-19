153a5e1389e52b305b0a34808196ffd0bf034cc6
William Østensen <william-98-@hotmail.com>
2019-02-19 10:56:59 +0100

    lagt inn start oppsett

--- pu_project/chefs_apprentice/views.py
+++ pu_project/chefs_apprentice/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Post
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+# @login_required
+def home(request):
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'chefs_apprentice/home.html', context)
+
+# def about(request):
+#     return HttpResponse("<h1>About Page</h1>")
+# @login_required
+def about(request):
+    return render(request, 'chefs_apprentice/about.html', {'title': 'About'})
