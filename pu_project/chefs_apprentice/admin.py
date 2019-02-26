5f7bc842858dfe9f0c304cc369705525a4c76200
krisfloc <krisfloc@stud.ntnu.no>
2019-02-26 11:41:26 +0100

    Updates ingreient and recipe

--- pu_project/chefs_apprentice/admin.py
+++ pu_project/chefs_apprentice/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import Post, Recipe, Ingredient
 
 # Register your models here.
-admin.site.register(Post)
+# admin.site.register(Post)
 admin.site.register(Recipe)
 admin.site.register(Ingredient)
--- pu_project/chefs_apprentice/views.py
+++ pu_project/chefs_apprentice/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
-from .models import Recipe
+from .models import Recipe, Post
 from django.contrib.auth.decorators import login_required
 
 @login_required
 def home(request):
     context = {
-        'Recipe': Recipe.objects.all()
+        'Recipe': Recipe.objects.all(),
+        'Post': Post.objects.all()
     }
     return render(request, 'chefs_apprentice/home.html', context)
 
