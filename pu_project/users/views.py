283a79e47c83cee453c8726d5a6f8948828019dd
katarinamurphy <katarina.g.murphy@gmail.com>
2019-03-07 11:13:15 +0100

    implementerte funksjonalitet for å endre passord

--- pu_project/users/views.py
+++ pu_project/users/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.forms import PasswordChangeForm
+
 
 
 
@@ -21,3 +24,19 @@ def register(request):
 @login_required
 def profile(request):
     return render(request, 'users/profile.html')
+
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)  # Important!
+            messages.success(request, 'Your password was successfully updated!')
+            return redirect('change_password')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'users/change_password.html', {
+        'form': form
+    })
