1a79526a19ac8277b4988367600c8e8dcb68b522
krisfloc <krisfloc@stud.ntnu.no>
2019-03-12 13:33:48 +0100

    Introduces test file for core functionality

--- pu_project/chefs_apprentice/tests.py
+++ pu_project/chefs_apprentice/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+from .models import Ingredient, Recipe, User, timezone
+
 
 # Create your tests here.
+class RecipeTest(TestCase):
+
+    def create_ingredient(self, name="test_ingredient"):
+        return Ingredient.objects.create(name=name)
+
+    def create_user(self, username="test_user", password="test_password"):
+        return User.objects.create(username=username, password=password)
+
+    def create_recipe(self, title="test_title", description="test_description"):
+        i = self.create_ingredient() #må legge til faktiske objekter i ingredients og author
+        u = self.create_user()
+        r = Recipe.objects.create(title=title, description=description, author=u)
+        r.ingredients.add(i)
+        return r
+
+    def test_recipe_creation(self):
+        r = self.create_recipe()
+        self.assertTrue(isinstance(r, Recipe)) #tester at r er en Recipe
+        self.assertTrue(isinstance(r.author, User)) #tester at r.author er en User
+        self.assertTrue(isinstance(r.ingredients.get(name="test_ingredient"), Ingredient)) #tester at r.ingredients inneholder ingrediensen vi ga den og at det er en ingrediens
+        self.assertEqual(r.visible, True) #tester at r.visible er satt til False
+        self.assertEqual("test_title", r.title) #testter at tittelen til r er den vi ga
+        self.assertEqual("test_user", r.author.username) #tester at brukernavnet til author er det vi ga
+        self.assertEqual("default.jpg", r.image.name) #tester at r.image er default.jpg
+        self.assertLessEqual(r.date_posted, timezone.now()) #tester at tiden r ble laget er nå eller før(siden vi kan teste over et minuttskifte)
