c4464778d0fc9c973c7ff437ca3abf95e754b44f
katarinamurphy <katarina.g.murphy@gmail.com>
2019-03-12 13:09:41 +0100

    downloaded recipes and favorite recipes

--- pu_project/chefs_apprentice/urls.py
+++ pu_project/chefs_apprentice/urls.py
@@ -6,7 +6,10 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.home, name='chef-home'),
     path('about/', views.about, name='chef-about'),
-    path('<str:recipetitle>/<pk>', views.view_recipe, name="view_recipe")
+    path('<str:recipetitle>/<pk>', views.view_recipe, name="view_recipe"),
+    path('downloadedrecipes/', views.downloadedrecipes, name= "downloadedrecipes"),
+    path('favoriterecipes/', views.favoriterecipes, name= "favoriterecipes")
 ]
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
