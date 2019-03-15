4b2fd1dbf220f0eacab9794357b57843dd9fb82a
krisfloc <krisfloc@stud.ntnu.no>
2019-03-15 16:10:31 +0100

    tries to fixe search for ChosenIngredient

--- pu_project/chefs_apprentice/views.py
+++ pu_project/chefs_apprentice/views.py
@@ -8,15 +8,15 @@ from django.core.paginator import Paginator
 
 @login_required
 def home(request):
-    queryset = Recipe.objects.all() # .order_by("-date_posted")
+    queryset = Recipe.objects.all()  # .order_by("-date_posted")
     recipe = queryset[0]
     i = recipe.ingredients.all()
-   #print(i)
+    # print(i)
     if request.GET.get("sortBy") == 'date':
-        queryset =  Recipe.objects.all().order_by("-date_posted")
+        queryset = Recipe.objects.all().order_by("-date_posted")
         context = {
-                'recipies': queryset
-                }
+            'recipies': queryset
+        }
         return render(request, 'chefs_apprentice/home.html', context)
     start_list = []
     for a in queryset:
@@ -29,11 +29,10 @@ def home(request):
 
     query = request.GET.get("q")
 
-
     if query:
         queryset = queryset.filter(
             Q(title__icontains=query)
-            )
+        )
 
     query_i = request.GET.get("q_i")
     if query_i:
@@ -42,37 +41,38 @@ def home(request):
         ingredients = list(set(ingredients))
         queryset = getRecipies(queryset, ingredients)
 
-
     if not query_i and not query:
         queryset = start_list
 
-    paginator = Paginator(queryset, 5) # Show 25 contacts per page
+    paginator = Paginator(queryset, 5)  # Show 25 contacts per page
 
     page = request.GET.get('page')
     queryset = paginator.get_page(page)
     context = {
-       #'posts': queryset,
+        # 'posts': queryset,
         'recipies': queryset
 
     }
     return render(request, 'chefs_apprentice/home.html', context)
+
 
 def getRecipies(queryset, ingredients):
     count = {}
     print(ingredients)
     for recipe in queryset:
         count[recipe] = 0
-        recipe_ingr = recipe.ingredients.all()
+        recipe_ingr = []
         print(recipe_ingr)
+        print(recipe)
         for i in recipe_ingr:
-   #    for i in recipe_ingr
             for j in ingredients:
-                if i.name==j:
+                if i.name == j:
                     count[recipe] += 1
         if count[recipe] == 0:
             del count[recipe]
+            print(recipe.title + ' deleted')
 
-   # Sorting by number of ingredients matched
+    # Sorting by number of ingredients matched
     sorted_count = sorted(count.items(), key=lambda kv: kv[1], reverse=True)
 
     n = len(sorted_count)
@@ -80,7 +80,7 @@ def getRecipies(queryset, ingredients):
     for i in range(n):
         temp = i
         if sorted_count[i][0].author.is_staff and i != 0:
-            counter = i-1
+            counter = i - 1
             for j in range(i, 0, -1):
                 if sorted_count[i][1] < sorted_count[counter][1]:
                     break
@@ -89,24 +89,25 @@ def getRecipies(queryset, ingredients):
         top.insert(temp, sorted_count[i][0])
     return top
 
-   # n = len(sorted_count)
-   #
-   # # Extracting top n recipies
-   # top = []
-   # for i in range(n):
-   #     # top.append(sorted_count[i][0])
-   #     temp = i
-   #     if sorted_count[i][0].author.is_staff and i != 0:
-   #         counter = i-1
-   #         for j in range(i,0,-1):
-   #             if sorted_count[i][0] < sorted_count[counter][1]:
-   #                 break
-   #          counter -= 1
-   #              temp -= 1
-   #      top.insert(temp, sorted_count[i][0])
-   #
-   #  return top
-   # List of recipies with highest match-count in descending order
+
+# n = len(sorted_count)
+#
+# # Extracting top n recipies
+# top = []
+# for i in range(n):
+#     # top.append(sorted_count[i][0])
+#     temp = i
+#     if sorted_count[i][0].author.is_staff and i != 0:
+#         counter = i-1
+#         for j in range(i,0,-1):
+#             if sorted_count[i][0] < sorted_count[counter][1]:
+#                 break
+#          counter -= 1
+#              temp -= 1
+#      top.insert(temp, sorted_count[i][0])
+#
+#  return top
+# List of recipies with highest match-count in descending order
 
 
 # def about(request):
@@ -115,23 +116,25 @@ def getRecipies(queryset, ingredients):
 def about(request):
     return render(request, 'chefs_apprentice/about.html', {'title': 'About'})
 
+
 def view_recipe(request, pk, recipetitle):
-    recipe=get_object_or_404(Recipe, pk=pk)
+    recipe = get_object_or_404(Recipe, pk=pk)
     context = {
-        "recipe":recipe,
+        "recipe": recipe,
     }
     return render(request, 'chefs_apprentice/recipe.html', context)
+
 
 def downloadedrecipes(request):
     return render(request, 'chefs_apprentice/downloadedrecipes.html', {'title': 'downloadedrecipes'})
 
+
 def favoriterecipes(request):
-        return render(request, 'chefs_apprentice/favoriterecipes.html', {'title': 'favoriterecipes'})
+    return render(request, 'chefs_apprentice/favoriterecipes.html', {'title': 'favoriterecipes'})
 
 
 def myrecipes(request):
-        return render(request, 'chefs_apprentice/myrecipes.html', {'title': 'myrecipes'})
-
+    return render(request, 'chefs_apprentice/myrecipes.html', {'title': 'myrecipes'})
 
 # class RecipeListView(ListView):
 #     model = Recipe
