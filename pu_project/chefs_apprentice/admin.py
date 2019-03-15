d3cd6156231cd0f2af282b16187831149ddb0e08
krisfloc <krisfloc@stud.ntnu.no>
2019-03-15 14:24:00 +0100

    Fixes Admin for ChosenIngredient

--- pu_project/chefs_apprentice/admin.py
+++ pu_project/chefs_apprentice/admin.py
@@ -1,18 +1,29 @@
 from django.contrib import admin
-from .models import Recipe, Ingredient
+from .models import Recipe, Ingredient, ChosenIngredient, timezone
 
-# Register your models here.
-# admin.site.register(Recipe)
+# Register your models here.)
 admin.site.register(Ingredient)
+admin.site.site_header = "Chef's Apprentice Admin"
+admin.site.site_title = "Chef's Apprentice Admin Portal"
+admin.site.index_title = "Welcome to Chef's Apprentice Admin Portal"
+
+
+class ChosenIngredientInLine(admin.TabularInline):
+    model = ChosenIngredient
 
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
     list_display = ("title", "visible", "author")
     actions = ["make_visible", "make_hidden"]
+    exclude = ('date_posted', 'ingredients')
+    inlines = [
+        ChosenIngredientInLine,
+    ]
 
     def make_visible(self, request, queryset):
         queryset.update(visible=True)
+        queryset.update(date_posted=timezone.now())
 
     def make_hidden(self, request, queryset):
         queryset.update(visible=False)
